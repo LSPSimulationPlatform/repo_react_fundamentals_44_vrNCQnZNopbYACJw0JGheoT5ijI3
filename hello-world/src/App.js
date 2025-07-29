760c5923da6bcb3ce62093f48136df3b2d86adb7
hasanmurad2 <m52552026@gmail.com>
2025-07-29 13:45:31 +0000

    initial hello

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <label style={{fontSize:"30px", color:"red"}}>This is my first React project</label>
+    </div>
+  );
+}
+
+export default App;
